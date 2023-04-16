0f14adda685f1a6de2f0a6da8df84bc1edb6af5c
Aditya Sinha <sinhadityaforever@gmail.com>
2023-04-16 22:54:20 +0530

    Server migrated to typescript

--- client/src/api/apiSetup.ts
+++ client/src/api/apiSetup.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-	baseURL: 'http://localhost:3000/user',
+	baseURL: 'http://localhost:3001/user',
 	headers: {
 		'Content-Type': 'application/json'
 	}
--- server/main.ts
+++ server/main.ts
@@ -1,29 +1,125 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import redis from 'redis';
-import { createClient } from 'redis';
+import { Entity, Schema, Client, Repository } from 'redis-om';
+import cors from 'cors';
 
-const client = createClient();
-client.on('error', (err) => console.log('Redis Client Error', err));
+//Redis-OM
+class User extends Entity {}
+let schema = new Schema(
+	User,
+	{
+		username: { type: 'string' },
+		secretKey: { type: 'string' },
+		numberOfWins: { type: 'number', sortable: true }
+	},
+	{ dataStructure: 'JSON' }
+);
+let client = await new Client().open();
 
-await client.connect();
+let userRepository = client.fetchRepository(schema);
+await userRepository.createIndex();
 
+//Express server
 const app = express();
-// const client = redis.createClient();
-
-// client.on('connect', () => {
-// 	console.log('Connected to Redis!');
-// });
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
 
-app.get('/user/', (req, res) => {
+app.get('/test/', (req: any, res: any) => {
 	res.status(200).json({
 		message: 'Success'
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Server started on port 3000!');
+app.post('/user', async (req: any, res: any) => {
+	const { username, secretKey } = req.body;
+	try {
+		const existingUser = await userRepository
+			.search()
+			.where('username')
+			.is.equalTo(username)
+			.return.first();
+		if (existingUser) {
+			//@ts-ignore
+			if (existingUser.secretKey == secretKey) {
+				res.status(201).json(existingUser);
+			} else {
+				res
+					.status(401)
+					.json({ message: 'Either user exists, or secretKey wrong' });
+			}
+		} else {
+			const user = await userRepository.createAndSave({
+				username,
+				secretKey,
+				numberOfWins: 0
+			});
+			res.status(200).json(user);
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({
+			error
+		});
+	}
 });
+
+app.post('/user/:id', async (req: any, res: any) => {
+	const id = req.params.id;
+	try {
+		let user = await userRepository.fetch(id);
+		if (!user) {
+			res.status(400).json({
+				message: 'No user exist'
+			});
+		} else {
+			//@ts-ignore
+			const wins = user.numberOfWins;
+			//@ts-ignore
+			user.numberOfWins = wins + 1;
+			await userRepository.save(user);
+			res.status(200).json({
+				message: 'Updated',
+				user
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(200).json({
+			message: 'Unknown error occurred'
+		});
+	}
+});
+
+app.get('/user', async (req: any, res: any) => {
+	try {
+		const users = await userRepository
+			.search()
+			// .sortBy('numberOfWins')
+			.sortDescending('numberOfWins')
+			.return.all();
+		//@ts-ignore
+		const filteredUsers = users.map(({ username, numberOfWins }) => ({
+			username,
+			numberOfWins
+		}));
+		res.status(200).json(filteredUsers);
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ message: 'Unknown error occurred' });
+	}
+});
+
+app.listen(3001, () => {
+	console.log('Server started on port 3001!');
+});
+
+export {};
